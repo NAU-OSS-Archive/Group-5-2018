b40c489ccb8f3a4f80a7e6eb111d36913ddfcbeb
carlimartinez <martinezcarli@gmail.com>
2018-09-23 09:56:31 -0700

    remove spaces from stirng

--- assignmentPR/F1.c
+++ assignmentPR/F1.c
@@ -10,6 +10,19 @@ int main( int argc, char *argv[] )
 
 bool characteristic(char numString[], int c)
 {
+  int index;
+  // Remove spaces
+  for(index=0; numString[index+1]!='\0'; index++)
+      while((numString[index]) == ' ') {
+      numString[index] = numString[index+1];
+      index++;
+  }
+
+  if(numString[0] - '1' > 3 && numString[1] - '0' > 5 && numString[2] - '0' > 1)
+  {
+    c = 351;
+    return false;
+  }
   return false;
 }
 
@@ -19,9 +32,15 @@ bool mantissa(char numString[], int numerator, int denominator) {
 
 int endIndex(char numString[])
 {
-  int index = 0;
+  int index;
 
-  
+  for(index = 0; numString[index] != '\0'; index++)
+  {
+
+  }
+
+  return 0;
 }
+
 
 #endif
--- assignmentPR/F1.h
+++ assignmentPR/F1.h
@@ -9,5 +9,8 @@ int main( int argc, char *argv[] );
 
 bool characteristic(char numString[], int c);
 bool mantissa(char numString[], int numerator, int denominator);
+int endIndex(char numString[]);
+void whitespace(char str[]);
+
 
 #endif
