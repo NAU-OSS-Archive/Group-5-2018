8b06d134c889434da5822abc48618965ff1726f2
carlimartinez <martinezcarli@gmail.com>
2018-09-23 17:56:41 -0700

    add function descriptions

--- assignmentPR/F1.h
+++ assignmentPR/F1.h
@@ -8,8 +8,11 @@
 
 int main( int argc, char *argv[] );
 
+// Checks if invalid and sets first digit to c
 bool characteristic(char numString[], int c);
+// Checks if invalid and assigns mantissa
 bool mantissa(char numString[], int numerator, int denominator);
+// Looks for characters that will cause conversion erros
 bool invalidCheck(char numString[]);
 
 
