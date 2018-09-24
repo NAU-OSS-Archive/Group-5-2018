bfec7b9f53d0e637b5a50eb32fd94730b418150c
carlimartinez <martinezcarli@gmail.com>
2018-09-23 17:19:46 -0700

    finish fleshing out characteristic method

--- assignmentPR/F1.c
+++ assignmentPR/F1.c
@@ -5,44 +5,77 @@
 
 int main( int argc, char *argv[] )
 {
-  printf("\n");
-  characteristic("1000", 0);
+  characteristic(" -505.00\0", 0);
   printf("\n");
 }
 
 bool characteristic(char numString[], int c)
 {
   int index;
-
-
-  if(numString[0] - '0' >= 1 && numString[1] - '0' >= 0 && numString[2] - '0' >= 0)
-  {
-    c = 351;
-  }
-
-  for(index=0; numString[index]!='\0'; index++)
-  {
-      // printf("%c", numString[index]);
-      // printf("%i", c);
-  }
-
-  return false;
-}
-
-bool mantissa(char numString[], int numerator, int denominator) {
-  return false;
-}
-
-int endIndex(char numString[])
-{
-  int index;
+  int newIndex = 0;
+  bool negative = false;
+  bool firstDigit = false;
+  char newNum[40] = "";
 
   for(index = 0; numString[index] != '\0'; index++)
   {
+      // Store only number and sign into new character array
+      if(numString[index] != ' ' && numString[index] != '+'
+        && numString[index] != '-')
+      {
+        // Establish what the first integer is and store into c
+        if(isdigit(numString[index]) != 0 && firstDigit == false)
+        {
+          c = numString[index] - '0';
+          firstDigit = true;
+        }
 
+        // Any character after the first number that isn't a decimal will mean
+        // the string is invalid
+        if(firstDigit == true && numString[index] != '.' && isdigit(numString[index] == 0))
+        {
+          printf("Invalid string");
+          return false;
+        }
+
+        newNum[newIndex] = numString[index];
+        newIndex++;
+      }
+
+      // Detect for letters, an obvious invalid string
+      if(isalpha(numString[index]) != 0)
+      {
+        printf("Invalid string");
+        return false;
+      }
+
+      if(numString[index] == '-')
+      {
+        negative = true;
+      }
   }
 
-  return 0;
+  if(negative == true)
+  {
+    c = -c;
+  }
+
+  printf("(c=%i)", c);
+  return true;
+}
+
+bool mantissa(char numString[], int numerator, int denominator) {
+
+  int m;
+
+  if(numString[0] - '0' >= 1 && numString[1] - '0' >= 0 && numString[2] - '0' >= 0)
+  {
+    m = 351;
+    printf("\n351");
+  }
+
+
+  return false;
 }
 
 
--- assignmentPR/F1.h
+++ assignmentPR/F1.h
@@ -4,13 +4,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main( int argc, char *argv[] );
 
 bool characteristic(char numString[], int c);
 bool mantissa(char numString[], int numerator, int denominator);
-int endIndex(char numString[]);
-void whitespace(char str[]);
 
 
 #endif
